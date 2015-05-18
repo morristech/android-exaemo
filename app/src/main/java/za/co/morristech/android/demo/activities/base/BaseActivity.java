136684244cdaf83efbd406342d97eeb25d2ec8bb
Wade Morris <wade.fbi@gmail.com>
2015-05-18 15:51:44 +0200

    Fixed up build.gradle and Added BaseActivity class.

--- app/src/main/java/za/co/morristech/android/demo/activities/base/BaseActivity.java
+++ app/src/main/java/za/co/morristech/android/demo/activities/base/BaseActivity.java
@@ -1,8 +1,21 @@
 package za.co.morristech.android.demo.activities.base;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import za.co.morristech.android.demo.R;
+
 /**
  * Created by Wade Morris.
  */
-public class BaseActivity {
+public class BaseActivity extends AppCompatActivity {
+
     private static final String TAG = BaseActivity.class.getName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.activity_base);
+    }
+
 }
