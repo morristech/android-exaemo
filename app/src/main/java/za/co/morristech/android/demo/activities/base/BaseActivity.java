7fdcad229ec93a7c9e145043e7f8d74f4feb2ecc
Wade Morris <wade.fbi@gmail.com>
2015-05-18 15:50:54 +0200

    Fixed up build.gradle signing configuration

---
+++ app/src/main/java/za/co/morristech/android/demo/activities/base/BaseActivity.java
@@ -0,0 +1,8 @@
+package za.co.morristech.android.demo.activities.base;
+
+/**
+ * Created by Wade Morris.
+ */
+public class BaseActivity {
+    private static final String TAG = BaseActivity.class.getName();
+}
